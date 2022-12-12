11f1115d19043e15af855fa0f093d73889a3fb21
Rvsharma12 <Ravi.s@antino.io>
2022-12-12 14:37:00 +0530

    final

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require('express')
 
 const app =express()
+app.use(express.json())
 
 
 // var FCM = require('fcm-node');
--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -2,45 +2,39 @@ let express = require('express')
 const fetch = require('node-fetch')
 const router = express.Router()
 
-
+var FCM = require('fcm-node');
+var serverKey = 'AAAAn3_JRCQ:APA91bHxzsIWqyM6LBMfm9IQ14yIZFUpmeMfIEy-dUWE0wS1hqmIqtpf_5PnzV1Po0J1IJtCpyRiHHyGtqMHH4kvq4EhbUI36mDBqOqYeXUbyZi4SNINsRyWrrR9zpBvXfwDNujREWHz';
+var fcm = new FCM(serverKey);
 
 
 router.route('/send').post(async(req,res)=>{
-    let notification ={
-        title:'notifications send',
-        subtitle:'first task'
-    }
-
-    let fcm_token = ['cdZyqL3vSIaQJeQ0B01wDI:APA91bE2MVGeV1GdvUtr_bCNcHg1bjWzgw4krXEg6HOleMFSwB48Gc3b_3VeJcXH_yJuJEgl5vlV-qgli9Ud5Xv07j6Clks9ZZS2hZNBwL1uisXRqK8AFQ60oe7jfob4NggGQtwJ77pu']
-
-    let notification_body ={
-
-        'notifcations':notification,
-
-        'regestration_ids':fcm_token
-    }
-
-    fetch('https://fcm.googleapis.com/fcm/send',{
-
-        'method':'Post',
-        'header':{
-            'Authorization':'keys'+'BC4JV9iJ03AZAU73KCiDjJOpPwhlIaHH3QfbpVty1fK5TXak-K36LhT1hbg4GOoioUHi1dDBRDz-PBOUTMwp_r4',
-            'Content-Type':'application/json'
-        },
-        'body':JSON.stringify(notification_body)
-    })
-    .then(()=>{
-
-        res.status(200).send('Notifications Send Sucessfully')
-      
-    })
-        
-    .catch((err)=>{
-
-        res.status(400).send('Something Wwent wrong')
-        console.log(err)
-        
-    })
+    var message = {
+        to:req.body.to,
+            notification: {
+                title: 'NotifcatioTestAPP',
+                body: '{"Message from node js app"}',
+            },
+    
+            data: { //you can send only notification or only data(or include both)
+                title: 'ok cdfsdsdfsd',
+                body: '{"name" : "okg ooggle ogrlrl","product_id" : "123","final_price" : "0.00035"}'
+            }
+    
+        };
+    
+        fcm.send(message, function(err, response) {
+            if (err) {
+                console.log("Something has gone wrong!"+err);
+                console.log("Respponse:! "+response);
+                returnres.status(500).json(err)
+            } else {
+                // showToast("Successfully sent with response");
+                console.log("Successfully sent with response: ", response);
+                return res.status(200).json(response)
+            }
+    
+        });
 })
+   
 
 module.exports =router
